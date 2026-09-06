74af631f57de6a105978d2d71af26d313ca5fc4d
Tamás Gálffy <ezittgtx@gmail.com>
2026-09-06 19:18:01 +0200

    fix(game): Don't spawn for server on listen (#650)
    Previously, each peer would spawn player avatars in Forest Brawl based
    on the peer list they knew of. However, there was no way for them to
    know if the server was running as dedicated, meaning the server peer
    itself would not play. In these cases, clients would spawn an avatar for
    the server, which would have no name, and would float in the air.
    
    This PR fixes this by moving the spawning logic to a
    `MultiplayerSpawner`, and only managing player spawns on the server.
    This is then correctly synchronized by the `MultiplayerSpawner` logic.
    
    While at it, also fixes the issue of the bootstrappers knowing about
    `BrawlerSpawner` specifically, which would result in errors when copying
    them to new projects.

--- examples/forest-brawl/scripts/brawler-spawner.gd
+++ examples/forest-brawl/scripts/brawler-spawner.gd
@@ -1,16 +1,17 @@
-extends Node
+extends MultiplayerSpawner
 class_name BrawlerSpawner
 
 @export var player_scene: PackedScene
-@export var spawn_root: Node
 @export var camera: FollowingCamera
 @export var joining_screen: Control
-@export var name_input: LineEdit
 
-var spawn_host_avatar: bool = true
 var avatars: Dictionary = {}
 
+var _logger := NetfoxLogger.new("fb", "BrawlerSpawner")
+
 func _ready():
+	spawn_function = _spawn
+
 	NetworkEvents.on_client_start.connect(_handle_connected)
 	NetworkEvents.on_server_start.connect(_handle_host)
 	NetworkEvents.on_peer_join.connect(_handle_new_peer)
@@ -20,31 +21,32 @@ func _ready():
 
 func _handle_connected(id: int):
 	if joining_screen:
-		joining_screen.visible = true
-
-	# Spawn an avatar for us
-	_spawn(id)
-
-	if joining_screen:
+		joining_screen.show()
 		await NetworkTime.after_sync
-		joining_screen.visible = false
+		joining_screen.hide()
 
 func _handle_host():
-	if spawn_host_avatar:
+	if not NetworkBootstrapper.is_dedicated_host():
 		# Spawn own avatar on host machine
-		_spawn(1)
+		spawn(1)
 
 func _handle_new_peer(id: int):
-	# Spawn an avatar for new player
-	var avatar = _spawn(id)
+	if not is_multiplayer_authority():
+		# Only spawn on server
+		return
 
-	# Hide avatar until player syncs time
-	avatar.visible = false
+	# Wait for player to sync time
 	while not NetworkTime.is_client_synced(id):
 		await NetworkTime.after_client_sync
-	avatar.visible = true
+
+	# Spawn an avatar for new player
+	spawn(id)
 
 func _handle_leave(id: int):
+	if not is_multiplayer_authority():
+		# Only despawn on server
+		return
+
 	if not avatars.has(id):
 		return
 
@@ -58,25 +60,24 @@ func _handle_stop():
 		avatar.queue_free()
 	avatars.clear()
 
-func _spawn(id: int) -> BrawlerController:
-	var avatar = player_scene.instantiate() as BrawlerController
-	avatars[id] = avatar
-	avatar.name += " #%d" % id
-	avatar.player_id = id
-	spawn_root.add_child(avatar)
+func _spawn(peer_id: int) -> BrawlerController:
+	var avatar := player_scene.instantiate() as BrawlerController
+	avatars[peer_id] = avatar
+	avatar.name += " #%d" % peer_id
+	avatar.player_id = peer_id
 
 	# Avatar is always owned by server
 	avatar.set_multiplayer_authority(1)
 
-	print("Spawned avatar %s at %s" % [avatar.name, multiplayer.get_unique_id()])
+	_logger.info("Spawned avatar %s at %s" % [avatar.name, multiplayer.get_unique_id()])
 
 	# Avatar's input object is owned by player
 	var input = avatar.find_child("Input")
 	if input != null:
-		input.set_multiplayer_authority(id)
-		print("Set input(%s) ownership to %s" % [input.name, id])
+		input.set_multiplayer_authority(peer_id)
+		_logger.debug("Set input(%s) ownership to %s" % [input.name, peer_id])
 
-	if id == multiplayer.get_unique_id():
+	if peer_id == multiplayer.get_unique_id():
 		# If avatar is own, assign it as camera follow target and emit event
 		camera.target = avatar
 		GameEvents.on_own_brawler_spawn.emit(avatar)
@@ -84,7 +85,7 @@ func _spawn(id: int) -> BrawlerController:
 		# Submit name
 		var settings := ForestBrawlSettings.get_active()
 		var player_name = NameProvider.name() if settings.randomize_name else settings.player_name
-		print("Submitting player name " + player_name)
+		_logger.debug("Submitting player name " + player_name)
 		_submit_name.rpc(player_name)
 
 	return avatar
@@ -94,4 +95,4 @@ func _submit_name(player_name: String):
 	var pid = multiplayer.get_remote_sender_id()
 	var avatar = avatars[pid]
 	avatar.player_name = player_name
-	print("Setting player name for #%s to %s" % [pid, player_name])
+	_logger.debug("Setting player name for #%s to %s" % [pid, player_name])
